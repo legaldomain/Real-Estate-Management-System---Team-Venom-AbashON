fd9b31d43ab7832db4005d468bdf13c45e3000e9
Sadaf <sadaf.priyo6216@gmail.com>
2023-11-22 01:03:21 +0600

    remove some icons from dropdown bar

--- resources/views/admin/body/header.blade.php
+++ resources/views/admin/body/header.blade.php
@@ -182,7 +182,7 @@
 							</div>
 						</li>
 
-            
+
             @php
             $id = Auth::user()->id;
             $profileData = App\Models\User::find($id);
@@ -210,18 +210,7 @@
                       <span>Profile</span>
                     </a>
                   </li>
-                  <li class="dropdown-item py-2">
-                    <a href="javascript:;" class="text-body ms-0">
-                      <i class="me-2 icon-md" data-feather="edit"></i>
-                      <span>Edit Profile</span>
-                    </a>
-                  </li>
-                  <li class="dropdown-item py-2">
-                    <a href="javascript:;" class="text-body ms-0">
-                      <i class="me-2 icon-md" data-feather="repeat"></i>
-                      <span>Switch User</span>
-                    </a>
-                  </li>
+                  
                   <li class="dropdown-item py-2">
                     <a href="{{ route('admin.logout')}}" class="text-body ms-0">
                       <i class="me-2 icon-md" data-feather="log-out"></i>
