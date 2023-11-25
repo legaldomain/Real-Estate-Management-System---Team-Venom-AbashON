359bde7767abd5ea86860a929156dbd84708ac04
Sadaf <sadaf.priyo6216@gmail.com>
2023-11-25 23:40:27 +0600

    property, multi image, facility table

--- database/migrations/2023_11_25_171336_create_properties_table.php
+++ database/migrations/2023_11_25_171336_create_properties_table.php
@@ -13,37 +13,34 @@ return new class extends Migration
     {
         Schema::create('properties', function (Blueprint $table) {
             $table->id();
-            $table->timestamps()->nullable();
-            $table->string('ptype_id->nullable()');
-            $table->string('amenitis_id')->nullable();
+            $table->string('ptype_id');
+            $table->string('facility_id');
             $table->string('property_name');
-            $table->string('property_slug')->nullable();
-            $table->string('amenitis_code')->nullable();
-            $table->string('property_status');
+            $table->string('property_slug');
+            $table->string('property_code');
+            $table->string('property_status')->nullable();
             $table->string('lowest_price')->nullable();
             $table->string('max_price')->nullable();
-            $table->string('property_thumbnail')->nullable();
-            $table->string('short_descp')->nullable();
+            $table->string('property_thumbnail');
+            $table->text('short_descp')->nullable();
             $table->text('long_descp')->nullable();
             $table->string('bedrooms')->nullable();
             $table->string('bathrooms')->nullable();
             $table->string('garage')->nullable();
-            $table->string('garage_size')->nullable();
             $table->string('property_size')->nullable();
             $table->string('property_video')->nullable();
             $table->string('address')->nullable();
             $table->string('city')->nullable();
             $table->string('state')->nullable();
-            $table->string('zip code')->nullable();
+            $table->string('postal_code')->nullable();
             $table->string('neighborhood')->nullable();
             $table->string('latitude')->nullable();
             $table->string('longitude')->nullable();
             $table->string('featured')->nullable();
             $table->string('hot')->nullable();
-            $table->integar('agent_id')->nullable();
+            $table->integer('agent_id')->nullable();
             $table->string('status')->default(0);
             $table->timestamps();
-            
         });
     }
 
