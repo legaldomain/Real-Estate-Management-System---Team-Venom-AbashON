78f6924d0e2d6000c3090bbe513720d6db82df24
Sadaf <sadaf.priyo6216@gmail.com>
2023-11-25 10:25:52 +0600

    property type - 1

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AgentController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\Backend\PropertyTypeController;
 
 
 
