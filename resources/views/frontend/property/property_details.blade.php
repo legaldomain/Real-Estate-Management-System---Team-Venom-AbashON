1e6c8c42f4c4ba65b4725b627cc515060de8cd52
Sadaf <sadaf.priyo6216@gmail.com>
2023-12-03 00:12:05 +0600

    map integration in every property dynamic

--- resources/views/frontend/property/property_details.blade.php
+++ resources/views/frontend/property/property_details.blade.php
@@ -134,13 +134,13 @@
                                     <div 
                                         class="google-map" 
                                         id="contact-google-map" 
-                                        data-map-lat="40.712776" 
-                                        data-map-lng="-74.005974" 
-                                        data-icon-path="assets/images/icons/map-marker.png"  
+                                        data-map-lat="{{ $property->latitude  }}" 
+                                        data-map-lng="{{ $property->longitude  }}" 
+                                        data-icon-path="{{asset('frontend/assets/images/icons/map-marker.png')}}"  
                                         data-map-title="Brooklyn, New York, United Kingdom" 
                                         data-map-zoom="12" 
                                         data-markers='{
-                                            "marker-1": [40.712776, -74.005974, "<h4>Branch Office</h4><p>77/99 New York</p>","assets/images/icons/map-marker.png"]
+                                            "marker-1": [40.712776, -74.005974, "<h4>Branch Office</h4><p>77/99 New York</p>","{{asset('frontend/assets/images/icons/map-marker.png')}}"]
                                         }'>
 
                                     </div>
